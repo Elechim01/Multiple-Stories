5eaee1ff2f81eba9b54aafb8162f95bf88e0d646
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-11 22:21:31 +0200

    Initial Commit

---
+++ Shared/Multiple_StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiple_StoriesApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Multiple_StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
